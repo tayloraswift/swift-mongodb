8b342335d9de0ab7551ce06b0726f9a360b597ba
taylorswift <kelvin13ma@gmail.com>
2023-05-14 21:47:18 +0000

    clarify Int32.encode(to:) documentation

--- Sources/BSONEncoding/Encodability/BSONEncodable.swift
+++ Sources/BSONEncoding/Encodability/BSONEncodable.swift
@@ -81,7 +81,9 @@ extension Int32:BSONEncodable
 }
 extension Int64:BSONEncodable
 {
-    /// Encodes this integer as a value of type ``BSON.int64``.
+    /// Encodes this integer as a value of type ``BSON.int64``. It will always use
+    /// the 64-bit representation, even if it would fit in a ``BSON.int32``. To use
+    /// a variable-length encoding, encode an ``Int`` instead.
     @inlinable public
     func encode(to field:inout BSON.Field)
     {
