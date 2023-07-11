45b8e4477b2f1143d5da46a5d4ec0ffa0d42cedc
taylorswift <kelvin13ma@gmail.com>
2023-07-11 00:00:50 +0000

    enable running test batteries at the top-level

--- Sources/MongoTesting/Mongo.DriverBootstrap (ext).swift
+++ Sources/MongoTesting/Mongo.DriverBootstrap (ext).swift
@@ -3,6 +3,25 @@ import MongoDB
 extension Mongo.DriverBootstrap
 {
     public
+    func run(_ tests:Tests, _ batteries:any MongoTestBattery...) async
+    {
+        await self.run(tests, batteries)
+    }
+    public
+    func run(_ tests:Tests, _ batteries:[any MongoTestBattery]) async
+    {
+        for battery:any MongoTestBattery in batteries
+        {
+            if  let tests:TestGroup = tests / battery.id
+            {
+                await self.run(tests, for: battery)
+            }
+        }
+    }
+}
+extension Mongo.DriverBootstrap
+{
+    public
     func run(_ tests:TestGroup, _ batteries:any MongoTestBattery...) async
     {
         await self.run(tests, batteries)
@@ -14,18 +33,28 @@ extension Mongo.DriverBootstrap
         {
             if  let tests:TestGroup = tests / battery.id
             {
-                await tests.do
-                {
-                    try await self.withTemporaryDatabase(.init(battery.id),
-                        logger: battery.logging.map { .init(level: $0) })
-                    {
-                        try await battery.run(tests, pool: $0, database: $1)
-                    }
-                }
+                await self.run(tests, for: battery)
             }
         }
     }
-
+}
+extension Mongo.DriverBootstrap
+{
+    private
+    func run(_ tests:TestGroup, for battery:any MongoTestBattery) async
+    {
+        await tests.do
+        {
+            try await self.withTemporaryDatabase(.init(battery.id),
+                logger: battery.logging.map { .init(level: $0) })
+            {
+                try await battery.run(tests, pool: $0, database: $1)
+            }
+        }
+    }
+}
+extension Mongo.DriverBootstrap
+{
     /// See ``Mongo.SessionPool.withTemporaryDatabase(_:run:)``.
     public
     func withTemporaryDatabase(_ database:Mongo.Database,
