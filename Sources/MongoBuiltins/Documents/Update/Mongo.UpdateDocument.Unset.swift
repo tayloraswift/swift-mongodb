2dbaddb1dfadddc7f4dba1ccec4255bf5c3056c6
taylorswift <kelvin13ma@gmail.com>
2023-07-18 21:39:48 +0000

    add a doc note about Mongo.UpdateDocument.Unset

--- Sources/MongoBuiltins/Documents/Update/Mongo.UpdateDocument.Unset.swift
+++ Sources/MongoBuiltins/Documents/Update/Mongo.UpdateDocument.Unset.swift
@@ -1,5 +1,8 @@
 extension Mongo.UpdateDocument
 {
+    /// Takes a document and removes the specified fields.
+    /// Not to be confused with the ``Mongo.PipelineState.Unset unset``
+    /// aggregation pipeline stage, which can take a field path directly.
     @frozen public
     enum Unset:String, Hashable, Sendable
     {
