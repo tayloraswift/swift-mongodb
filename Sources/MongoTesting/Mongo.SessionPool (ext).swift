d335282d9cfa1bb23097345e9067245344760767
taylorswift <kelvin13ma@gmail.com>
2023-09-10 18:13:39 -0500

    require scratch database names to start with uppercase letters, and allow tests to create additional databases within the test body (#98)

--- Sources/MongoTesting/Mongo.SessionPool (ext).swift
+++ Sources/MongoTesting/Mongo.SessionPool (ext).swift
@@ -2,9 +2,13 @@ import MongoDB
 
 extension Mongo.SessionPool
 {
-    /// Sets up a temporary database with the specified name, removing it after
-    /// the closure argument returns. If a database with the specified name
-    /// already exists, this function *drops* it before calling the closure.
+    /// Sets up a temporary database with the specified name, removing it after the closure
+    /// argument returns. If a database with the specified name already exists, this function
+    /// *drops* it before calling the closure.
+    ///
+    /// It is good practice to give scratch databases names that start with an uppercase letter.
+    /// To prevent accidental deletion of long-lived databases, this function always traps if
+    /// the database name begins with a lowercase letter.
     ///
     /// >   Note:
     ///     MongoDB database creation is lazy, generally MongoDB servers will
@@ -14,12 +18,29 @@ extension Mongo.SessionPool
     func withTemporaryDatabase(_ database:Mongo.Database,
         run body:() async throws -> ()) async throws
     {
-        //  if we already have a database with this name, drop it.
+        guard case true? = database.name.first?.isUppercase
+        else
+        {
+            fatalError("""
+                scratch database names must start with an uppercase letter to prevent accidental
+                deletion of long-lived databases!
+                """)
+        }
+        //  If we already have a database with this name, drop it. This often happens if tests
+        //  crash midway through.
         try await self.run(command: Mongo.DropDatabase.init(), against: database)
 
         let before:Set<Mongo.Database> = .init(try await self.run(
             command: Mongo.ListDatabases.NameOnly.init(),
             against: .admin))
+
+        if  before.contains(database)
+        {
+            fatalError("""
+                failed to remove pre-existing database '\(database)', it is possible this test
+                is running in parallel with another test using the same database name!
+                """)
+        }
 
         try await body()
 
@@ -29,10 +50,14 @@ extension Mongo.SessionPool
             command: Mongo.ListDatabases.NameOnly.init(),
             against: .admin))
 
-        guard before == after
-        else
+        //  We cannot assert equivalence of `before` and `after` because `body` might set up
+        //  additional databases.
+        if  after.contains(database)
         {
-            fatalError("failed to remove temporary database '\(database)'")
+            fatalError("""
+                failed to remove temporary database '\(database)', it is possible this test
+                is running in parallel with another test using the same database name!
+                """)
         }
     }
 }
