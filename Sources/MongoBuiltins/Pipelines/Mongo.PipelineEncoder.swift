c9bb812f428e82b5828e2339636fc88bd293d99b
taylorswift <mdianna1989@gmail.com>
2024-03-26 19:38:45 +0000

    lint deprecated APIs

--- Sources/MongoBuiltins/Pipelines/Mongo.PipelineEncoder.swift
+++ Sources/MongoBuiltins/Pipelines/Mongo.PipelineEncoder.swift
@@ -32,24 +32,6 @@ extension Mongo.PipelineEncoder:BSON.Encoder
 }
 extension Mongo.PipelineEncoder
 {
-    /// This is a legacy source compatibility aid that does nothing and will be deprecated soon.
-    @available(*, deprecated, message: "use subscripts instead")
-    @inlinable public mutating
-    func stage(_ populate:(inout Self) throws -> ()) rethrows
-    {
-        try populate(&self)
-    }
-}
-extension Mongo.PipelineEncoder
-{
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Out) -> Mongo.Collection?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Out) -> Mongo.Collection?
     {
@@ -68,15 +50,6 @@ extension Mongo.PipelineEncoder
                 $0[.out] = value
             }
         }
-    }
-
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Out) -> Mongo.Namespaced<Mongo.Collection>?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
     }
 
     @inlinable public
@@ -98,16 +71,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Merge) -> Mongo.MergeDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Merge) -> Mongo.MergeDocument?
     {
@@ -128,16 +95,9 @@ extension Mongo.PipelineEncoder
         }
     }
 }
+
 extension Mongo.PipelineEncoder
 {
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Bucket) -> Mongo.BucketDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Bucket) -> Mongo.BucketDocument?
     {
@@ -157,16 +117,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.BucketAuto) -> Mongo.BucketAutoDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.BucketAuto) -> Mongo.BucketAutoDocument?
     {
@@ -186,16 +140,9 @@ extension Mongo.PipelineEncoder
             }
         }
     }
-
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Count) -> Mongo.AnyKeyPath?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+}
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Count) -> Mongo.AnyKeyPath?
     {
@@ -241,14 +188,6 @@ extension Mongo.PipelineEncoder
 
 extension Mongo.PipelineEncoder
 {
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.CollectionStats) -> Mongo.CollectionStatsDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
     @inlinable public
     subscript(stage key:Mongo.Pipeline.CollectionStats) -> Mongo.CollectionStatsDocument?
     {
@@ -268,16 +207,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.CurrentOperation) -> Mongo.CurrentOperationDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.CurrentOperation) -> Mongo.CurrentOperationDocument?
     {
@@ -297,23 +230,20 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Densify) -> Never?
     {
         nil
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript<Array>(key:Mongo.Pipeline.Documents) -> Array? where Array:BSONEncodable
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript<Array>(stage key:Mongo.Pipeline.Documents) -> Array? where Array:BSONEncodable
     {
@@ -333,16 +263,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Facet) -> Mongo.FacetDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Facet) -> Mongo.FacetDocument?
     {
@@ -362,37 +286,37 @@ extension Mongo.PipelineEncoder
             }
         }
     }
-
+}
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Fill) -> Never?
     {
         nil
     }
-
+}
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.GeoNear) -> Never?
     {
         nil
     }
-
+}
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.GraphLookup) -> Never?
     {
         nil
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Group) -> Mongo.GroupDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Group) -> Mongo.GroupDocument?
     {
@@ -412,16 +336,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.IndexStats) -> [String: Never]?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.IndexStats) -> [String: Never]?
     {
@@ -441,16 +359,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Limit) -> Int?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Limit) -> Int?
     {
@@ -470,29 +382,28 @@ extension Mongo.PipelineEncoder
             }
         }
     }
-
+}
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.ListLocalSessions) -> Never?
     {
         nil
     }
+}
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.ListSessions) -> Never?
     {
         nil
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Lookup) -> Mongo.LookupDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Lookup) -> Mongo.LookupDocument?
     {
@@ -512,16 +423,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Match) -> Mongo.PredicateDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Match) -> Mongo.PredicateDocument?
     {
@@ -541,16 +446,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.PlanCacheStats) -> [String: Never]?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.PlanCacheStats) -> [String: Never]?
     {
@@ -570,16 +469,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Project) -> Mongo.ProjectionDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Project) -> Mongo.ProjectionDocument?
     {
@@ -599,17 +492,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript<RedactMode>(key:Mongo.Pipeline.Redact) -> RedactMode?
-        where RedactMode:BSONEncodable
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript<RedactMode>(stage key:Mongo.Pipeline.Redact) -> RedactMode?
         where RedactMode:BSONEncodable
@@ -630,17 +516,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript<Document>(key:Mongo.Pipeline.ReplaceWith) -> Document?
-        where Document:BSONEncodable
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript<Document>(stage key:Mongo.Pipeline.ReplaceWith) -> Document?
         where Document:BSONEncodable
@@ -662,14 +541,6 @@ extension Mongo.PipelineEncoder
         }
     }
 
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.ReplaceWith) -> Mongo.SetDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
     @inlinable public
     subscript(stage key:Mongo.Pipeline.ReplaceWith) -> Mongo.SetDocument?
     {
@@ -689,16 +560,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Sample) -> Mongo.SampleDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Sample) -> Mongo.SampleDocument?
     {
@@ -718,16 +583,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Set) -> Mongo.SetDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Set) -> Mongo.SetDocument?
     {
@@ -747,15 +606,20 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
+extension Mongo.PipelineEncoder
+{
     @available(*, unavailable, message: "unimplemented")
     @inlinable public
     subscript(stage key:Mongo.Pipeline.SetWindowFields) -> Never?
     {
         nil
     }
+}
 
-
+extension Mongo.PipelineEncoder
+{
     @available(*, deprecated, renamed: "subscript(stage:)")
     @inlinable public
     subscript(key:Mongo.Pipeline.ShardedDataDistribution) -> [String: Never]?
@@ -783,16 +647,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Skip) -> Int?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Skip) -> Int?
     {
@@ -812,16 +670,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Sort) -> Mongo.SortDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Sort) -> Mongo.SortDocument?
     {
@@ -841,17 +693,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript<GroupKey>(key:Mongo.Pipeline.SortByCount) -> GroupKey?
-        where GroupKey:BSONEncodable
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript<GroupKey>(stage key:Mongo.Pipeline.SortByCount) -> GroupKey?
         where GroupKey:BSONEncodable
@@ -872,16 +717,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.UnionWith) -> Mongo.Collection?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.UnionWith) -> Mongo.Collection?
     {
@@ -901,15 +740,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.UnionWith) -> Mongo.UnionWithDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.UnionWith) -> Mongo.UnionWithDocument?
     {
@@ -929,16 +763,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Unset) -> Mongo.AnyKeyPath?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Unset) -> Mongo.AnyKeyPath?
     {
@@ -959,13 +787,6 @@ extension Mongo.PipelineEncoder
         }
     }
 
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Unset) -> [Mongo.AnyKeyPath]
-    {
-        get { [ ] }
-        set (value) { self[stage: key] = value }
-    }
     /// Does nothing if the assigned array is empty.
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Unset) -> [Mongo.AnyKeyPath]
@@ -990,16 +811,10 @@ extension Mongo.PipelineEncoder
             }
         }
     }
+}
 
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Unwind) -> Mongo.AnyKeyPath?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
-    }
-
+extension Mongo.PipelineEncoder
+{
     @inlinable public
     subscript(stage key:Mongo.Pipeline.Unwind) -> Mongo.AnyKeyPath?
     {
@@ -1019,14 +834,6 @@ extension Mongo.PipelineEncoder
                 $0[.unwind] = value
             }
         }
-    }
-
-    @available(*, deprecated, renamed: "subscript(stage:)")
-    @inlinable public
-    subscript(key:Mongo.Pipeline.Unwind) -> Mongo.UnwindDocument?
-    {
-        get { nil }
-        set (value) { self[stage: key] = value }
     }
 
     @inlinable public
