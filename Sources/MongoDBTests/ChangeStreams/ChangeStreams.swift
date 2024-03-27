8730bea43099abd08a94a56f1d4e741dc5e1b592
taylorswift <mdianna1989@gmail.com>
2024-03-27 00:36:12 +0000

    more-powerful change streams test

--- Sources/MongoDBTests/ChangeStreams/ChangeStreams.swift
+++ Sources/MongoDBTests/ChangeStreams/ChangeStreams.swift
@@ -2,12 +2,51 @@ import BSON
 import MongoDB
 import MongoTesting
 
+extension ChangeStreams
+{
+    struct Ticket
+    {
+        let id:Int
+        let value:String
+
+        init(id:Int, value:String)
+        {
+            self.id = id
+            self.value = value
+        }
+    }
+}
+extension ChangeStreams.Ticket:MongoMasterCodingModel
+{
+    enum CodingKey:String, Sendable
+    {
+        case id = "_id"
+        case value = "v"
+    }
+}
+extension ChangeStreams.Ticket:BSONDocumentEncodable
+{
+    func encode(to bson:inout BSON.DocumentEncoder<CodingKey>)
+    {
+        bson[.id] = self.id
+        bson[.value] = self.value
+    }
+}
+extension ChangeStreams.Ticket:BSONDocumentDecodable
+{
+    init(bson:BSON.DocumentDecoder<CodingKey>) throws
+    {
+        self.init(id: try bson[.id].decode(), value: try bson[.value].decode())
+    }
+}
+
 struct ChangeStreams<Configuration>:MongoTestBattery where Configuration:MongoTestConfiguration
 {
     static
     func run(tests:TestGroup, pool:Mongo.SessionPool, database:Mongo.Database) async throws
     {
-        let collection:Mongo.Collection = "watchable"
+        let collection:Mongo.Collection = "tickets"
+
         await tests.do
         {
             let session:Mongo.Session = try await .init(from: pool)
@@ -15,6 +54,9 @@ struct ChangeStreams<Configuration>:MongoTestBattery where Configuration:MongoTe
                 command: Mongo.Aggregate<Mongo.Cursor<BSON.Document>>.init(collection,
                     writeConcern: .majority,
                     readConcern: .majority,
+                    //  This is always needed, otherwise the cursor will die after a fixed
+                    //  amount of time.
+                    tailing: .init(timeout: 5_000, awaits: true),
                     stride: 10)
                 {
                     $0[stage: .changeStream] { _ in }
@@ -22,9 +64,39 @@ struct ChangeStreams<Configuration>:MongoTestBattery where Configuration:MongoTe
                 against: database,
                 by: .now.advanced(by: .seconds(15)))
             {
-                for try await _:[BSON.Document] in $0
+                var poll:Int = 0
+                for try await batch:[BSON.Document] in $0
                 {
-                    return
+                    defer
+                    {
+                        poll += 1
+                    }
+                    if  poll == 0
+                    {
+                        tests.expect(batch.count ==? 0)
+                    }
+                    else if
+                        let document:BSON.Document = batch.first
+                    {
+                        print(document)
+                        return
+                    }
+                    else if poll > 5
+                    {
+                        //  If more than 5 polling intervals have passed and we still haven't
+                        //  received any documents, then the test has failed.
+                        tests.expect(true: false)
+                    }
+
+                    let _:Task<Void, any Error> = .init
+                    {
+                        let session:Mongo.Session = try await .init(from: pool)
+
+                        let document:Ticket = .init(id: 1, value: "a")
+                        let _:Mongo.InsertResponse = try await session.run(
+                            command: Mongo.Insert.init(collection, encoding: [document]),
+                            against: database)
+                    }
                 }
             }
         }
